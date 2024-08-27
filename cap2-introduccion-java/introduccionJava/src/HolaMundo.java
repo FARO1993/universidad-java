b34fcd2d0de0d00e79790c3270627f62f60aeace
Facundo Rodriguez <facundo.m.rodriguez1993@gmail.com>
2024-08-27 12:49:39 -0300

    cap2 commit

---
+++ cap2-introduccion-java/introduccionJava/src/HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+    //Agregamos el metodo principal (main)
+    public static void main(String[] args) {
+        //System.out.println("Hello World !!!");
+        //Reto - Presentate con Java.
+        System.out.println("Facundo Martin Rodriguez");
+        System.out.println("31");
+        System.out.println("Argentina");
+    }
+}
